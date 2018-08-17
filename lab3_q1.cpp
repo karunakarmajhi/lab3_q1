704377760d4a9f9e70ce01d3adbef6db3d717e3f
karunakarmajhi <42205167+karunakarmajhi@users.noreply.github.com>
2018-08-17 06:03:59 +0000

    Add files via upload

---
+++ lab3_q1.cpp
@@ -0,0 +1,19 @@
+// library 
+#include<iostream>
+using namespace std;
+// write the main function 
+int main(){
+// declaration of basic data types
+int a=1;
+char b=2;
+float c=1.002;
+double d=1.02;
+bool e=0;
+// sizes of the variables
+cout <<"size of int;"<<sizeof(int)<<"byte "<<"contents of a ="<<a<<endl;
+cout <<"size of char;"<<sizeof(char)<<"byte "<<"contents of b="<<b<<endl;
+cout <<"size of float;"<<sizeof(float)<<"byte "<<"contents of c="<<c<<endl;
+cout <<"size of double;"<<sizeof(double)<<"byte "<<"contents of d="<<d<<endl;
+cout <<"size of bool;"<<sizeof(bool)<<"byte "<<"contents of e= "<<d<<endl;
+return 0;
+}
